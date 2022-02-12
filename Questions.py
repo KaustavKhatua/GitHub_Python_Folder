7ef6e76128902c527b6506825e3a8e9bddbc3ad8
Kaustav <kaustavkhatua98@gmail.com>
2022-02-12 10:44:12 +0530

    Questions 1

---
+++ Questions.py
@@ -0,0 +1,49 @@
+## Fundamental data types and storing one value into a variable.
+# Store an integer value into a variable named integer_variable.
+# Store an float value into a variable named float_variable.
+# Store an string into a variable named string_variable.
+# Store an boolean value into a variable named boolean_variable.
+
+## Type casting
+# Convert float_variable to integer and print the result.
+# Convert boolean_variable to integer and print the result.
+# Add integer_variable and boolean_variable. Print the result and explain why this result came.
+
+## Some operations on integer.
+# Take an integer from user and store it in a variable.
+# Add 2 to the variable and print the result.
+# Calculate the remainder when the variable is divided by 5.
+# Calculate square of the value stored in the variable.
+
+## Collection type of data types and storing multiple values into a variable.
+# Create a set of 5 elements.
+# Create a list of 7 elements.
+# Take a number from user and add this number at the last of the list.
+# Print length of the list.
+
+## Some operations on list.
+# Store a list of length 5 in a variable.
+# Print second element of the list.
+# Calculate sum of second and third element of the list.
+# Calculate product of first and fifth element.
+# Print last element of the list.
+# Print second last element of the list.
+
+## For loop
+# Print 0 to 10.
+# Print 1 to 10.
+# Print 10 to 14.
+# Take an integer from user and print from 0 upto that digit. For example: if user gives input 5 then, print 0 to 5 in console.
+# Take an integer from user and print from 1 upto that digit. For example: if user gives input 5 then, print 1 to 5 in console.
+# Take an integer from user and print from that digit to 1. For example if user gives input 5 then in console print 5, 4, 3, 2, 1.
+# Take an integer from user. Calculate sum of 1 to that digit. For example if user gives input 5 then sum of 1, 2, 3, 4, 5.
+# Take an integer from user. Calculate product of 1 to that digit. For example if user gives input 5 then product of 1, 2, 3, 4, 5.
+# Take an integer from user. Print multiplication table for that number.
+# Take an integer from user. Print factorial of that number.
+
+## If-else condition
+# Take a number from user. If the number is greater than 5 print "Number is greater than 5".
+# Take a string from user. If the string is equal to 'A' print "You have written A".
+
+## While loop.
+# Do all the questions in for loop using while loop.
