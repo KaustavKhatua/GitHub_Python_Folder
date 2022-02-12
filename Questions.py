d1262532c9b26dcd5e31f01fefd709bc18d05a5c
Kaustav <kaustavkhatua98@gmail.com>
2022-02-12 22:05:25 +0530

    Questions 2

--- Questions.py
+++ Questions.py
@@ -1,49 +1,31 @@
-## Fundamental data types and storing one value into a variable.
-# Store an integer value into a variable named integer_variable.
-# Store an float value into a variable named float_variable.
-# Store an string into a variable named string_variable.
-# Store an boolean value into a variable named boolean_variable.
-
-## Type casting
-# Convert float_variable to integer and print the result.
-# Convert boolean_variable to integer and print the result.
-# Add integer_variable and boolean_variable. Print the result and explain why this result came.
-
-## Some operations on integer.
-# Take an integer from user and store it in a variable.
-# Add 2 to the variable and print the result.
-# Calculate the remainder when the variable is divided by 5.
-# Calculate square of the value stored in the variable.
-
-## Collection type of data types and storing multiple values into a variable.
-# Create a set of 5 elements.
-# Create a list of 7 elements.
-# Take a number from user and add this number at the last of the list.
-# Print length of the list.
-
-## Some operations on list.
-# Store a list of length 5 in a variable.
-# Print second element of the list.
-# Calculate sum of second and third element of the list.
-# Calculate product of first and fifth element.
-# Print last element of the list.
-# Print second last element of the list.
-
-## For loop
-# Print 0 to 10.
-# Print 1 to 10.
-# Print 10 to 14.
-# Take an integer from user and print from 0 upto that digit. For example: if user gives input 5 then, print 0 to 5 in console.
-# Take an integer from user and print from 1 upto that digit. For example: if user gives input 5 then, print 1 to 5 in console.
-# Take an integer from user and print from that digit to 1. For example if user gives input 5 then in console print 5, 4, 3, 2, 1.
-# Take an integer from user. Calculate sum of 1 to that digit. For example if user gives input 5 then sum of 1, 2, 3, 4, 5.
-# Take an integer from user. Calculate product of 1 to that digit. For example if user gives input 5 then product of 1, 2, 3, 4, 5.
-# Take an integer from user. Print multiplication table for that number.
-# Take an integer from user. Print factorial of that number.
+# Take two numbers as input. Interchange them.
 
 ## If-else condition
-# Take a number from user. If the number is greater than 5 print "Number is greater than 5".
-# Take a string from user. If the string is equal to 'A' print "You have written A".
+## Note: You are allowed to use simple condition (ie. only one condition at a time), for example: using & or | is not allowed. Also say to 
+## ckeck whether a number is between 2 and 10 you cannot use 2 <= a <= 10 (because this also checks two conditions one for 2 <= a, anoter for a <= 10). 
+## You are only allowed to write one condition at a time.
 
-## While loop.
-# Do all the questions in for loop using while loop.
+# Take two numbers from user, store them in varibles named purchase_price and selling_price. Suppose a seller has bought an item in purchase_price and sold it
+# in selling_price. Print whether he has gained or lossed also the percentage gain or loss. Example: Suppose the purchase_price is 100 and selling_price is 110.
+# Output should be "10% gain". Hint: use formatted string.
+
+# Any quadratic equation is of the form a.x^2 + b.x + c = 0. Take three numbers as input, store these in a, b, c. Check whether the roots are real or imaginary.
+# Print "Real" if the roots are real, else print "Imaginary".
+
+# Take two numbers as input. Find maximum between these two numbers.
+
+# Take three numbers as input. Find minimum between these three numbers.
+
+# Take three number as input. Find difference between the largest and smallest number.
+
+# Take a number as input. Print "Less than 10" if number is less than 10, print "Between 11 and 20" if number is between 11 and 20, print 
+# "Between 21 and 30" if number is between 21 and 30, print "Between 31 and 40" if number is between 31 and 40 (remember you are not allowed to use two <= in
+# one condition).
+
+# Take three numbers as input. Print "Triangle" if the three numbers can form a triangle, print "Not triangle" if they do not form a triangle.
+
+# Take four numbers as input. Print "Square" if the four numbers form a  square, print "Not Square" if they do not form a square.
+
+
+## for loop questions.
+# Print -10 to -1.
